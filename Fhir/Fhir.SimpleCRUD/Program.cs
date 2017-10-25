6e1ebb41e86b72c13ecfce3fc1cc92a6c40e9733
Rosando Desouza <desouza.rosando@gmail.com>
2017-10-25 16:33:06 +0530

    1. Added gitignore file 2. Create new visual studio solution with a console application 3. Added functionality to create a patient

---
+++ Fhir/Fhir.SimpleCRUD/Program.cs
@@ -0,0 +1,99 @@
+﻿using Hl7.Fhir.Model;
+using Hl7.Fhir.Rest;
+using Hl7.Fhir.Serialization;
+using Hl7.Fhir.Support;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Linq;
+
+namespace Fhir.SimpleCRUD
+{
+    public class Program
+    {
+        public static string FhirClientEndPoint = "https://fhirtest.uhn.ca/baseDstu3/";
+
+        public static void Main(string[] args)
+        {
+            Patient patient = CreatePatient("John", "Doe", new DateTime(1992, 12, 19));
+        }
+
+        public static Patient CreatePatient(string given, string family, DateTime birthDate)
+        {
+            Patient patient = new Patient();
+            Patient responsePatient = new Patient();
+
+            //Set patient name
+            patient.Name = new List<HumanName>();
+
+            HumanName patientName = new HumanName();
+
+            patientName.Use = HumanName.NameUse.Official;
+            patientName.Prefix = new string[] { "Mr" };
+
+            //Default way using properties to set Given and Family name
+            //patientName.Given = new string[] { given };
+            //patientName.Family = family;
+
+            //Using methods to sets the Given and Family name
+            patientName.WithGiven(given).AndFamily(family);
+
+            patient.Name.Add(patientName);
+
+            //Set patient Identifier
+            patient.Identifier = new List<Identifier>();
+
+            Identifier patientIdentifier = new Identifier();
+
+            patientIdentifier.System = "http://someurl.net/not-sure-about-the-value/1.0";
+            patientIdentifier.Value = Guid.NewGuid().ToString();
+
+            patient.Identifier.Add(patientIdentifier);
+
+            //Set patient birth date
+            patient.BirthDate = birthDate.ToFhirDate();
+
+            //Set Active Flag
+            patient.Active = true;
+            
+            try
+            {
+                LogToFile("Creating Patient");
+
+                LogToFile("Request: ");
+                var patientXml = FhirSerializer.SerializeResourceToXml(patient);
+                LogToFile(XDocument.Parse(patientXml).ToString());
+
+                FhirClient fhirClient = new FhirClient(FhirClientEndPoint);
+                responsePatient = fhirClient.Create(patient);
+
+                LogToFile("Response: ");
+                var responsePatientXml = FhirSerializer.SerializeResourceToXml(responsePatient);
+                LogToFile(XDocument.Parse(responsePatientXml).ToString());
+            }
+            catch(Exception ex)
+            {
+                LogToFile(ex.ToString());
+            }
+
+            return responsePatient;
+        }
+        
+        public static void LogToFile(string xml)
+        {
+            string path = Directory.GetCurrentDirectory() + "\\log.txt";
+
+            if (!File.Exists(path))
+            {
+                var file = File.Create(path);
+                file.Dispose();
+            }
+
+            using (StreamWriter sw = new StreamWriter(path, true))
+            {
+                sw.Write(xml);
+                sw.Write("\n\n");
+            }
+        }
+    }
+}
